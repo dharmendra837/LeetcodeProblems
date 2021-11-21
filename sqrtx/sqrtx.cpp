a4c42d7d7090c2c62e9e0166dbda2a18ff071566
dharmendra837 <57292177+dharmendra837@users.noreply.github.com>
2021-11-21 08:36:20 +0530

    Time: 51 ms (5.13%), Space: 5.9 MB (25.06%) - LeetHub

---
+++ sqrtx/sqrtx.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0)return 0;
+        
+        for(long long int i=1;i<=x;i++)
+        {
+            if(i*i==x)return i;
+            if(i*i >x)return i-1;
+        }
+        return -1;
+    }
+};
